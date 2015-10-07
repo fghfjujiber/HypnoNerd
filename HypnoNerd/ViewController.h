def8f6f1e92bda59c79db1f8d1e3fbc9cc903471
Xiaoming Chen <xiaoming_chen10@sina.com>
2015-10-07 20:17:17 +0800

    Initial Commit

---
+++ HypnoNerd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HypnoNerd
+//
+//  Created by Xiaoming Chen on 15/10/7.
+//  Copyright © 2015年 Jackchenxm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
